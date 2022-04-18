bafb6b61ade8c7c4aaf19dba930ca59bad63c7c3
minhtri06ltr <laptopdienthoai1@gmail.com>
2022-04-18 08:10:48 +0000

    send post loading

--- client/components/Input.jsx
+++ client/components/Input.jsx
@@ -10,7 +10,13 @@ import "emoji-mart/css/emoji-mart.css";
 import { Picker } from "emoji-mart";
 import { useDetect } from "../hooks/useDetectHook";
 import { db, storage } from "../firebase";
-import { addDoc, collection, updateDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const Input = () => {
@@ -66,7 +72,9 @@ const Input = () => {
   };
   return (
     <div
-      className={`border-b border-gray-700 p-4 flex space-x-4 overflow-y-auto`}
+      className={`border-b border-gray-700 p-4 flex space-x-4 overflow-y-auto ${
+        loading && "opacity-60"
+      }`}
     >
       <img
         className=" rounded-full h-14 w-14 object-cover cursor-pointer"
@@ -87,11 +95,13 @@ const Input = () => {
           {file && (
             <div className="relative">
               <div
-                className="absolute w-6 h-6 bg-[#15181c]
+                className={`absolute w-6 h-6 bg-[#15181c]
                hover:bg-[#272c26] bg-opacity-75 
                rounded-full flex items-center
-                justify-center top-1 right-1  cursor-pointer"
-                onClick={() => setFile(null)}
+                justify-center top-1 right-1  cursor-pointer ${
+                  loading && "cursor-default"
+                }`}
+                onClick={() => !loading && setFile(null)}
               >
                 <XIcon color="white" width={30} height={30} />
               </div>
@@ -103,7 +113,11 @@ const Input = () => {
           )}
         </div>
         <div className="flex items-center justify-between pt-2.5">
-          <div className="flex items-center">
+          <div
+            className={`flex items-center cursor-pointer ${
+              loading && " cursor-default"
+            }`}
+          >
             <div className="icon" onClick={() => imgInputRef.current.click()}>
               <PhotographIcon color="#1b9df0" width={25} />
               <input
@@ -111,12 +125,16 @@ const Input = () => {
                 onChange={addImgToPost}
                 ref={imgInputRef}
                 hidden
+                disabled={loading}
               />
             </div>
             <div className="icon ">
               <ChartBarIcon color="#1b9df0" width={23} />
             </div>
-            <div className="icon" onClick={() => setShowEmoji(!showEmoji)}>
+            <div
+              className="icon"
+              onClick={() => !loading && setShowEmoji(!showEmoji)}
+            >
               <EmojiHappyIcon color="#1b9df0" width={23} />
             </div>
             <div className="icon">
@@ -140,13 +158,33 @@ const Input = () => {
           </div>
           <button
             disabled={!input.trim() && !file}
-            className="text-white rounded-full bg-[#1d9bf0]
-             px-6 font-bold shadow-2xl py-1.5 hover:bg-[#1a8cd8]
+            className={`text-white rounded-full bg-[#1d9bf0]
+             w-28 font-bold shadow-2xl py-1.5 hover:bg-[#1a8cd8]
              disabled:opacity-50 disabled:hover:bg-[#1d9bf0]
-             "
-            onClick={sendPost}
+             flex items-center justify-center shrink-0
+             ${loading && "cursor-default"}`}
+            onClick={!loading ? sendPost : () => {}}
           >
-            Tweet
+            {loading ? (
+              <svg
+                role="status"
+                className=" w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                ></path>
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                ></path>
+              </svg>
+            ) : (
+              "Tweet"
+            )}
           </button>
         </div>
       </div>
